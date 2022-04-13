75f206580127041f7afba356762bfd345c4b67f0
Colk-tech <colk.tech@gmail.com>
2022-04-13 11:22:47 +0900

    add: Generate skelton code from the UML diagram

---
+++ 02/src/Reminder.java
@@ -0,0 +1,22 @@
+//
+// Created by Manaki ITO on 2022/04/13.
+// 伊藤 愛基 ( #2019024 )
+//
+
+public class Reminder {
+
+    private string name;
+
+    private int priority;
+
+    private ReminderList reminderList;
+
+    public Reminder() {
+
+    }
+
+    public void show() {
+
+    }
+
+}
---
+++ 02/src/ReminderList.java
@@ -0,0 +1,30 @@
+//
+// Created by Manaki ITO on 2022/04/13.
+// 伊藤 愛基 ( #2019024 )
+//
+
+public class ReminderList {
+
+    private Reminder[] reminder;
+
+    public ReminderList() {
+
+    }
+
+    public void show() {
+
+    }
+
+    public void add(int i, Reminder r) {
+
+    }
+
+    public void remove(int i) {
+
+    }
+
+    public void clear() {
+
+    }
+
+}
