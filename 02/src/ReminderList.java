39ebcd6e775074c21d1876726d5f740f8ddd1997
Colk-tech <colk.tech@gmail.com>
2022-04-13 11:47:39 +0900

    add: Comple the assignment '2-3'

--- 02/src/ReminderList.java
+++ 02/src/ReminderList.java
@@ -4,19 +4,32 @@
 //
 
 public class ReminderList {
-
     private Reminder[] reminder;
 
     public ReminderList() {
-
+        this.reminder = new Reminder[10];
     }
 
     public void show() {
+        System.out.println(
+                "----------Reminder List----------"
+        );
+        for (int i = 0; i < this.reminder.length; i++) {
+            if (this.reminder[i] == null) {
+                continue;
+            }
 
+            System.out.print(i + ": ");
+            this.reminder[i].show();
+        }
+        System.out.println(
+                "---------- End of List ----------"
+        );
+        System.out.println();
     }
 
     public void add(int i, Reminder r) {
-
+        this.reminder[i] = r;
     }
 
     public void remove(int i) {
