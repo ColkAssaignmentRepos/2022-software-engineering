27e89fb1834c6932ac9e300fa2fd1e9ac13eaf7b
Colk-tech <colk.tech@gmail.com>
2022-04-20 11:40:47 +0900

    add: Copy `Reminder` and `ReminderList` class from the previous assignment

---
+++ 03/src/ReminderList.java
@@ -0,0 +1,43 @@
+//
+// Created by Manaki ITO on 2022/04/13.
+// 伊藤 愛基 ( #2019024 )
+//
+
+public class ReminderList {
+    private Reminder[] reminder;
+
+    public ReminderList() {
+        this.reminder = new Reminder[10];
+    }
+
+    public void show() {
+        System.out.println(
+                "----------Reminder List----------"
+        );
+        for (int i = 0; i < this.reminder.length; i++) {
+            if (this.reminder[i] == null) {
+                continue;
+            }
+
+            System.out.print(i + ": ");
+            this.reminder[i].show();
+        }
+        System.out.println(
+                "---------- End of List ----------"
+        );
+        System.out.println();
+    }
+
+    public void add(int i, Reminder r) {
+        this.reminder[i] = r;
+    }
+
+    public void remove(int i) {
+        this.reminder[i] = null;
+    }
+
+    public void clear() {
+        this.reminder = new Reminder[10];
+    }
+
+}
