bd7f31d560bcb79fac03a1a64f03d604afd2ca70
Colk-tech <colk.tech@gmail.com>
2022-05-18 11:16:49 +0900

    add: Complete the assignment 5-3

--- 05/src/Controller.java
+++ 05/src/Controller.java
@@ -30,6 +30,16 @@ public class Controller {
         //引数で渡されたリマインダ情報をインスタンスにsetする
         r1.setName(n);
         r1.setPriority(p);
+
+        // 課題 5-3 作業 1
+        //リマインダの配列を取得する
+        Reminder[] l = reminderList.getReminder();
+        //i番目にインスタンスが存在している場合は、そこから空いている場所まで追加場所を後にずらす
+        //最後まで空きがない場合は、配列の最後に追加する(上書きされる場合もある)
+        if(l[i] != null) {
+            while(l[i]!=null && i<l.length-1) i++;
+        }
+
         //i番目にリマインダを追加する
         reminderList.add(i, r1);
     }
