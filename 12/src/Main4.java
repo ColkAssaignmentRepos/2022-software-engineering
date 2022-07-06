f9a0e00341d4e1a5a1218cceeb71baf0548c8cfe
Colk-tech <colk.tech@gmail.com>
2022-07-06 11:35:32 +0900

    add: Complete the `12-3` assignment

--- 12/src/Main4.java
+++ 12/src/Main4.java
@@ -1,6 +1,23 @@
-//ここに自分の名前
+//
+// Edited by Manaki ITO on 2022/07/06.
+// 伊藤 愛基 ( #2019024 )
+//
+
 public class Main4 {
     public static void main(String[] args) {
+        //Observerを作る
+        ObserverBarGraph observerBarGraph = new ObserverBarGraph();
+        ObserverLineGraph observerLineGraph = new ObserverLineGraph();
+        //Subjectを作る
+        SubjectData subjectData = new SubjectData();
 
+        //ObserverからSubjectを参照する
+        observerBarGraph.setSubjectData(subjectData);
+        observerLineGraph.setSubjectData(subjectData);
+        //SubjectにObserverを追加する
+        subjectData.addObserver(observerBarGraph);
+        subjectData.addObserver(observerLineGraph);
+        //各Observerを更新する
+        subjectData.notifyObserver();
     }
 }
